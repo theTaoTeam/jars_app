0483e9e5ec659ff0edf8ec56e59bb593518401cf
Scott Clampet <scottclampet@Scotts-MacBook-Pro-2.local>
2019-01-13 12:44:54 -0700

    feat(getrecipe and static drawer menu): built static getrecipe screen and added side drawer with all
    re #12

---
+++ fude_app/lib/pages/home/get-recipe/get_recipe.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:fude/scoped-models/main.dart';
+import 'package:fude/helpers/categories.dart';
+
+class GetRecipePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _GetRecipeState();
+  }
+}
+
+class _GetRecipeState extends State<GetRecipePage> {
+  String isSelected = categories[0];
+  Color selectedColor = Colors.white;
+  Color notSelectedColor = Colors.black;
+  final FixedExtentScrollController scrollController =
+      FixedExtentScrollController(initialItem: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModelDescendant<MainModel>(
+        builder: (BuildContext context, Widget child, MainModel model) {
+      return Container(
+        child: CupertinoPicker(
+          children: <Widget>[
+            Text(
+              categories[0],
+              style: TextStyle(
+                  color: isSelected == categories[0]
+                      ? selectedColor
+                      : notSelectedColor),
+            ),
+            Text(
+              categories[1],
+              style: TextStyle(
+                  color: isSelected == categories[1]
+                      ? selectedColor
+                      : notSelectedColor),
+            ),
+            Text(
+              categories[2],
+              style: TextStyle(
+                  color: isSelected == categories[2]
+                      ? selectedColor
+                      : notSelectedColor),
+            )
+          ],
+          diameterRatio: 1,
+          useMagnifier: true,
+          magnification: 2,
+          backgroundColor: Colors.transparent,
+          itemExtent: 30.0,
+          onSelectedItemChanged: (int newVal) {
+            print('changed category to: ' + categories[newVal]);
+            setState(() {
+              isSelected = categories[newVal];
+            });
+          },
+          scrollController: scrollController,
+        ),
+      );
+    });
+  }
+}
--- fude_app/lib/pages/home/home.dart
+++ fude_app/lib/pages/home/home.dart
@@ -1,31 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 import 'package:fude/pages/home/main_view.dart';
+import 'package:fude/scoped-models/main.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: Color.fromRGBO(162, 146, 199, 0.8),
-        iconTheme: IconThemeData(
-          color: Colors.white, //change your color here
+    return ScopedModelDescendant<MainModel>(
+        builder: (BuildContext context, Widget child, MainModel model) {
+      return Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Color.fromRGBO(162, 146, 199, 0.8),
+          iconTheme: IconThemeData(
+            color: Colors.white, //change your color here
+          ),
         ),
-      ),
-      body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            colors: <Color>[
-              const Color.fromRGBO(162, 146, 199, 0.8),
-              const Color.fromRGBO(51, 51, 63, 0.9),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text('email'),
+                accountEmail: Text(model.currentUser.email),
+              ),
+              Container(
+                child: ListTile(
+                  title: Text('favorites'),
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  title: Text('all recipes'),
+                ),
+              ),
             ],
-            stops: [0.2, 1.0],
-            begin: const FractionalOffset(0.0, 0.0),
-            end: const FractionalOffset(0.0, 1.0),
-          )),
-          child: MainView()),
-    );
+          ),
+        ),
+        body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: <Color>[
+                const Color.fromRGBO(162, 146, 199, 0.8),
+                const Color.fromRGBO(51, 51, 63, 0.9),
+              ],
+              stops: [0.2, 1.0],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(0.0, 1.0),
+            )),
+            child: MainView()),
+      );
+    });
   }
 }
